c27e15d9d68559e7289d5817e96dd1bebbce936c
Katsiaryna Mandryk <Mandrik20@gmail.com>
2023-04-08 15:48:01 +0300

    Initial commit

---
+++ src/com/app/Application.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class Application {
+
+    public static void main(String[] args) {
+            ConsoleReader consoleReader =new ConsoleReader();
+            consoleReader.getNumber();
+            consoleReader.getName();
+            consoleReader.getArray();
+        }
+    }
---
+++ src/com/app/ConsoleReader.java
@@ -0,0 +1,49 @@
+package com.app;
+
+import java.util.Scanner;
+
+public class ConsoleReader {
+
+    public void getName() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите имя:");
+        try {
+            if (console.nextLine().equals("Вячеслав")) {
+                System.out.println("Привет, Вячеслав");
+            } else {
+                System.out.println("Нет такого имени");
+            }
+        } catch (Exception e) {
+            System.out.println("Введено некорректное значение!");
+        }
+    }
+
+    public void getNumber() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите номер:");
+        try {
+            if (console.nextInt() > 7) {
+                System.out.println("Привет");
+            }
+        } catch (Exception e) {
+            System.out.println("Введено некорректное значение!");
+        }
+    }
+
+    public void getArray() {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите массив чисел:");
+        try {
+            String array = console.nextLine();
+            int[] number = new int[array.length()];
+            for (int i = 0; i < array.length(); i++) {
+                number[i] = Integer.parseInt(array.substring(i, i + 1));
+                if (number[i] == 3) {
+                    System.out.print(number[i]);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Введено некорректное значение!");
+        }
+    }
+}
